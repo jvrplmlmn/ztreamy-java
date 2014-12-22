558b45cb2a282e9504e7db8d86c1ef9d1283ced8
Javier Palomo Almena <jvrplmlmn@gmail.com>
2014-12-22 23:26:29 +0100

    Process a CLI argument as the publication URL

--- src/main/java/org/ztreamy/Publisher.java
+++ src/main/java/org/ztreamy/Publisher.java
@@ -26,9 +26,21 @@ public class Publisher {
     }
 
     public static void main(String[] args) throws IOException {
-        Publisher publisher = new Publisher(new URL("http://localhost:9000/events/publish"));
+        // Check arguments
+        if (args.length < 1 || args.length > 2) {
+            System.err.println("CLI parameter expected");
+            System.err.println("java Publisher <publication URL>");
+            System.exit(1);
+        }
+
+        // Publisher
+        Publisher publisher = new Publisher(new URL(args[0]));
+
+        // Event
         String sourceID = Event.createUUID();
         Event event = new Event(sourceID, "text/plain", "ztreamy-java-test");
+
+        // Publish event and process the result
         int result = publisher.publish(event);
         if (result == 200) {
             System.out.println("An event has just been sent to the server");
